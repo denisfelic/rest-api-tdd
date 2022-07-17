8f3e5da992b91433467e90fddcd6e0ca9a77d9db
Denis Feliciano <denisf.salles@gmail.com>
2022-07-17 00:15:52 -0300

    feat: add breeze api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\API\BooksController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,14 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-});
-
-Route::name('books.')->group(function () {
-    Route::get('/books', [BooksController::class, 'index'])->name('index');
-    Route::post('/books', [BooksController::class, 'store'])->name('store');
-    Route::get('/books/{book}', [BooksController::class, 'show'])->name('show');
-    Route::put('/books/{book}', [BooksController::class, 'update'])->name('update');
-    Route::delete('/books/{book}', [BooksController::class, 'delete'])->name('delete');
 });
